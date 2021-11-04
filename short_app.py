5002a8665a44cbe8496bec830b64327a361a2dc3
Olga Melnikova <shelga.melnikova@gmail.com>
2021-11-04 15:21:32 +0000

    index

--- short_app.py
+++ short_app.py
@@ -1,20 +1,17 @@
-from flask import Flask, render_template, redirect, request
+from flask import Flask, render_template, redirect, request, url_for, jsonify
 import sqlite3
 import shortuuid
-
+import socket
 
 app = Flask(__name__)
 
 
-@app.route('/', methods = ["GET", "POST"])
+@app.route('/', methods = ["POST"])
 ## index page
 
-def index():
+def get_url():
     if request.method == "POST":
         urlToSort = request.form.get("urlToSort")
-
-        if not urlToSort:
-            return render_template("apology.html", message = "You must provide a URL")
 
         shotrUrl = shortuuid.uuid(urlToSort)[:8]
 
@@ -30,13 +27,31 @@ def index():
 
         cursor.execute("INSERT INTO url_shortner (url, hash) VALUES (?, ?)", (urlToSort, shotrUrl))
         connect.commit()
+
+        print(request.environ['SERVER_NAME'])
+
+        # jsonobj = jsonify({'short URL': shotrUrl})
+        # return jsonobj
+
+        return 'Hello, world! running on %s' % request.Host
         
-        return render_template("result.html")
-
     if request.method == "GET":
-        return render_template("/index.html")
+        ## get hash for url
+        # ...
+        return redirect(url_for('app.show_result', hash=hash))
 
-# @app.route('/result', methods = ["GET", "POST"])
-# ## result page 
-# def hello():
-#     return render_template("/result.html")
+
+
+@app.route('/result/<hash>', methods = ["GET"])
+## result page 
+def show_result(hash):
+    pass
+
+
+
+if __== '__main__':
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock.bind(('localhost', 0))
+    port = sock.getsockname()[1]
+    sock.close()
+    app.run(port=port)
