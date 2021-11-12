b6b3d1e6d679bb6b3a115e1b9349bb8792081cc6
Olga Melnikova <shelga.melnikova@gmail.com>
2021-11-12 18:10:30 +0000

    GET is done

--- short_app.py
+++ short_app.py
@@ -1,10 +1,7 @@
-from flask import Flask, render_template, redirect, request, url_for, jsonify
+from flask import Flask, redirect, request, jsonify
 import sqlite3
 import shortuuid
-import socketserver
-import socket
 
-from urllib.parse import urlparse, urlunparse
 
 app = Flask(__name__)
 
@@ -12,13 +9,10 @@ app = Flask(__name__)
 
 @app.route('/', methods = ["POST"])
 ## index page
-## done
-
 def get_url():
     if request.method == "POST":
         urlToSort = request.form.get("urlToSort")
        
-
         shotrUrl = shortuuid.uuid(urlToSort)[:8]
 
         connect = sqlite3.connect("project.db")
@@ -28,20 +22,14 @@ def get_url():
             url TEXT,
             hash TEXT 
         )""")
-
         connect.commit()
 
         cursor.execute("INSERT INTO url_shortner (url, hash) VALUES (?, ?)", (urlToSort, shotrUrl))
         connect.commit()
-
         
         varToJson = f"{request.root_url}/result/" + shotrUrl
-        print("root_url", request.root_url)
-        print("urlToSort", urlToSort)
-        print("shotrUrl", shotrUrl)
 
         jsonobj = jsonify({'short URL': varToJson})
-        print("jsonobj", jsonobj)
   
         return jsonobj
 
@@ -52,16 +40,16 @@ def get_url():
 @app.route('/result/<varToJson>', methods = ["GET"])
 def show_result(varToJson):
     
-    varToJson = {varToJson}
+    varToJson = varToJson
     print("varToJson", varToJson)
 
     connect = sqlite3.connect("project.db")
     cursor = connect.cursor()
 
-    # longUrl= cursor.execute('SELECT url FROM url_shortner WHERE hash = "QBB2S5qm";')
+    # longUrl= cursor.execute('SELECT url FROM url_shortner WHERE hash = "LKEWWrV5";')
     longUrl= cursor.execute('SELECT url FROM url_shortner WHERE hash = ?', (varToJson,))
 
-    longUrl = cursor.fetchall()
+    longUrl = cursor.fetchone()
     print("longUrl", longUrl)
 
 
@@ -69,10 +57,6 @@ def show_result(varToJson):
   
 
     return redirect(longUrl[0], 302)
-
-
-## get longUrl
-
 
 
 if __name__ == '__main__':
