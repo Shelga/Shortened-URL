6f83395564bd36fa49f01cc9b0ded1ade173d08a
Olga Melnikova <shelga.melnikova@gmail.com>
2021-12-14 15:21:01 +0000

    changes

--- short_app.py
+++ short_app.py
@@ -14,15 +14,17 @@ from flask_migrate import Migrate
 import os
 import re
 
+load_dotenv()
+
 uri = os.getenv("DATABASE_URL")  # or other relevant config var
 if uri.startswith("postgres://"):
     uri = uri.replace("postgres://", "postgresql://", 1)
+
 # rest of connection code using the connection string `uri`
 
 app = Flask(__name__)
 
 ## take environment variables from .env.
-load_dotenv()
 uri = os.getenv('DATABASE_URL')
 if uri.startswith("postgres://"):
     uri = uri.replace("postgres://", "postgresql://", 1)
@@ -76,7 +78,6 @@ def get_url():
     
             return jsonobj
 
-
 @app.route('/result/<varToJson>', methods = ["GET"])
 def show_result(varToJson):
     
@@ -88,64 +89,6 @@ def show_result(varToJson):
 
     ## Redirect to long URL
     return redirect(str(longUrl[0]), 302)
-
-
-# @app.route('/', methods = ["POST"])
-# def get_url():
-#     if request.method == "POST":
-#         # Receive URL
-#         urlToSort = request.form.get("urlToSort")
-
-#         try:
-#             res = requests.get(urlToSort)
-#             correct_url = True
-#         except:
-#             return "Please enter a valid URL. Perhaps you miss http:// or https://"
-
-#         if correct_url:
-            
-#             ## Calling the hash function from the module
-#             shotrUrl = get_hash(urlToSort)
-
-#             ## Create taable and save long url and hash
-#             connect = sqlite3.connect("project.db")
-#             cursor = connect.cursor()
-#             cursor.execute("""CREATE TABLE IF NOT EXISTS url_shortner(
-#                 id INTEGER PRIMARY KEY,
-#                 url TEXT,
-#                 hash TEXT 
-#             )""")
-#             connect.commit()
-
-#             cursor.execute("INSERT INTO url_shortner (url, hash) VALUES (?, ?)", (urlToSort, shotrUrl))
-#             connect.commit()
-            
-#             ## Create short URL 
-#             varToJson = f"{request.root_url}/result/" + shotrUrl
-
-#             jsonobj = jsonify({'short URL': varToJson})
-    
-#             return jsonobj
-
-
-# @app.route('/result/<varToJson>', methods = ["GET"])
-# def show_result(varToJson):
-    
-#     ## Get hash from short URL 
-#     varToJson = varToJson
-
-#     connect = sqlite3.connect("project.db")
-#     cursor = connect.cursor()
-
-#     longUrl= cursor.execute('SELECT url FROM url_shortner WHERE hash = ?', (varToJson,))
-
-#     longUrl = cursor.fetchone()
-
-#     ## Redirect to long URL
-
-#     return redirect(longUrl[0], 302)
-    
-
 
 if __name__ == '__main__':
     print("APP IS STARTING")
